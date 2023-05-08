f6d81e4fd8d47b78cf67cbecaf7689fd6923286d
GOPINISAIBHUVAN <gopinisaibhuvan@gmail.com>
2023-05-08 21:31:33 +0530

    first graded module

---
+++ src/HelloGoodbye.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class HelloGoodbye {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String name1 = sc.nextLine();
+    String name2 = sc.nextLine();
+        System.out.println("Goodbye " + name1 + " and " + name2 );
+    }
+}
